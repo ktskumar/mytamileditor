edb98e7e014c2be1eb5ab764f62b95805c1d29f5
Shantha Kumar <ktskumar@hotmail.com>
2019-10-31 09:15:45 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
+import Plain from 'slate-plain-serializer';
+import { Editor } from 'slate-react'
+
 import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
 
-function App() {
+const initialValue = Plain.deserialize(
+  'This is editable plain text, just like a <textarea>!'
+)
+
+
+class App extends React.Component {
+
+  state = {
+    value: initialValue,
+    word:''
+  }
+
+  constructor(props){
+    super(props);
+    
+
+  }
+
+  onChange = ({ value }) => {
+    this.setState({ value });
+
+    
+  }
+
+
+  onKeyDown = (event, editor, next) => {
+
+    if(event.key != " "){
+      this.setState({
+        word:this.state.word +event.key
+      });
+      
+    }
+
+    
+    fetch('https://www.google.com/inputtools/request?text='+event.key+'&ime=transliteration_en_ta&num=5&cp=0&cs=0&ie=utf-8')
+	.then(function(response) {
+		if (response.status >= 400) {
+			throw new Error("Bad response from server");
+		}
+		return response.json();
+	})
+	.then(function(stories) {
+		console.log(stories);
+  });
+  
+    // Return with no changes if the keypress is not '&'
+    if (event.key !== ' ') return next()
+
+    // Prevent the ampersand character from being inserted.
+    event.preventDefault()
+
+    // Change the value by inserting 'and' at the cursor's position.
+    editor.insertText(' space ')
+
+
+  }
+
+  
+
+  render(){
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Editor
+        placeholder="Enter some plain text..."
+        
+        value={this.state.value}
+        onChange={this.onChange}
+        onKeyDown={this.onKeyDown}
+      />
     </div>
   );
+  }
 }
 
 export default App;
